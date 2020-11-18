dba3767011f58e428879b4b6db9c4a005edc2e3d
Shawn Flanigan <shawnmflanigan@gmail.com>
2020-11-18 10:47:38 -0800

    add arrays for password selection

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// All arrays for password selection
+let lowerCase =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+let upperCase =['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+let num =[1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+let specChar =['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '=', '<', '>'];
+
