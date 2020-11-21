d975a953ae49a063bbd68e519e277d968ea6920c
Shawn Flanigan <shawnmflanigan@gmail.com>
2020-11-21 14:55:52 -0800

    add for loop to populate generated password.

--- script.js
+++ script.js
@@ -18,20 +18,20 @@ let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 
 let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+let num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
 let specChar = ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '=', '<', '>'];
 
 // Empty arrays
 
 let allChars = [];
-allChars = '';
-
+let blank = [];
+let charLimit = 0;
 // Password Character length function
 
 function passCharLength () {
-   charLimit =(prompt('Please select the number of characters you would like for your password. Please choose between 8-128.'));
-  if (8 > charLimit && charLimit < 128) {
+   charLimit =parseInt(prompt('Please select the number of characters you would like for your password. Please choose between 8-128.'));
+  if (charLimit <8 || charLimit > 128) {
     alert('Please try again with 8 through 128 characters for your password');
     passCharLength();
   }
@@ -64,4 +64,17 @@ function generatePassword () {
   }
   console.log(allChars);
   passCharLength();
+  console.log(charLimit);
+
+  // Generate random from blank array
+  let final = '';
+  for (let i = 0; i < charLimit; i++) {
+    console.log(final);
+    blank = Math.floor(Math.random () * allChars.length -1);
+    final += allChars[blank];
+  }
+
+  return final;
+
+  
 }  
